2ce193be506b7db2842b0a53440e0adb349bf603
LuisTerranova <linharesdev43@gmail.com>
2025-06-26 17:22:35 -0400

    Added swagger

--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -1,25 +1,33 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Todo.Api.Data;
+using Todo.Api.Handlers;
+using Todo.Core.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add Identity services with default user (IdentityUser) and role (IdentityRole)
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
+
+builder.Services.AddTransient<IChoreHandler, ChoreHandler>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
 {
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 8;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireLowercase = true;
-}) //Add EntityFrameworkStores to connect ef and identity to database
-.AddEntityFrameworkStores<AppDbContext>()
-.AddDefaultTokenProviders();
+    x.CustomSchemaIds(n => n.FullName); //Melhor para documentar metodos de nomes repetidos
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
--- Todo.Api/obj/Debug/net9.0/Todo.Api.AssemblyInfo.cs
+++ Todo.Api/obj/Debug/net9.0/Todo.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6d65c9bee1ee3358577c5427c24c527fab8f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57bb277cb88b547581fb00f9739f272e087cf27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
